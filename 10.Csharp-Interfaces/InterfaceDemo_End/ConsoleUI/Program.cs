1d4b048044a55921e919f3c8bd8176f3a5d934c3
Joshhortt <joshhortt@yahoo.com>
2021-01-29 20:08:04 +0000

    Add final draft of Console Aplication about Interfaces.

---
+++ 10.Csharp-Interfaces/InterfaceDemo_End/ConsoleUI/Program.cs
@@ -0,0 +1,55 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IProductModel> cart = AddSampleData();
+            CustomerModel customer = GetCustomer();
+
+            foreach (IProductModel prod in cart)
+            {
+                prod.ShipItem(customer);
+
+                if (prod is IDigitalProductModel digital)
+                {
+                    Console.WriteLine($"For the { digital.Title } you have { digital.TotalDownloadsLeft } downloads left.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static CustomerModel GetCustomer()
+        {
+            return new CustomerModel
+            {
+                FirstName = "Josh",
+                LastName = "Hortt",
+                City = "Lagos",
+                EmailAddress = "joshhortt@yahoo.com",
+                PhoneNumber = "911-815-877"
+            };
+        }
+
+        private static List<IProductModel> AddSampleData()
+        {
+            List<IProductModel> output = new List<IProductModel>();
+
+            output.Add(new PhysicalProductModel { Title = "Nerd Football" });
+            output.Add(new PhysicalProductModel { Title = "Joshhortt T-Shirt" });
+            output.Add(new PhysicalProductModel { Title = "Hard Drive" });
+            output.Add(new DigitalProductModel { Title = "Lesson Source Code" });
+            output.Add(new CourseProductModel { Title = ".Net Console Aplicatios" });
+
+            return output;
+        }
+    }
+}
